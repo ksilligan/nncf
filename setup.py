017917883f5ee84f57a64ec7ae8bcd68d14eb992
Roman Khristoforov <romanleonhristoforov@gmail.com>
2023-06-19 16:49:34 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@
 # instead. This doesn't fit us as we currently want to do custom stuff during
 # installation such as setting version based on the commit SHA for repo-based
 # installs.
-# test123
+
 
 import codecs
 import glob
